82c09029babe3eb96c3c5f703c92eb4986771c4e
Parth Patel 2 <201501015@daiict.ac.in>
2022-01-27 21:17:08 +0530

    Backend - Seed manager for creating first manager

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,4 +1,22 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import User from '../models/user.model';
+
+const seedManager = async () => {
+  const managerUsername = process.env.DEFAULT_MANAGER_USERNAME;
+  const managerPassword = process.env.DEFAULT_MANAGER_PASSWORD;
+
+  const manager = await User.findOne({username: managerUsername});
+  if (!manager) {
+    const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10);
+    const passwordHash = await bcrypt.hash(managerPassword, saltRounds);
+    await User.create({
+      username: managerUsername,
+      password: passwordHash,
+      role: 'manager',
+    });
+  }
+};
 
 const connectDb = async () => {
   await mongoose.connect(process.env.MONGODB_URI, {
@@ -9,6 +27,7 @@ const connectDb = async () => {
 
 const applyDatabaseSetup = async () => {
   await connectDb();
+  await seedManager();
 };
 
 export default applyDatabaseSetup;
