7e8e8942e2894fbb8b5a28159a75d8a43564886e
Parth Patel 2 <201501015@daiict.ac.in>
2022-01-30 22:45:59 +0530

    Frontend - Wired pages in App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import './App.css';
+import {Switch} from 'react-router-dom';
+import AppRoute from './components/common/AppRoute';
+
+import Navbar from './components/common/Navbar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import ReserveTab from './pages/reserve/ReserveTab';
+import Signup from './pages/signup/Signup';
+import AuthProvider from './providers/AuthProvider';
+import {AUTHENTICATED, GUEST} from './utils/constant';
 
 function App() {
-  return null;
+  return (
+    <AuthProvider>
+      <Navbar />
+      <Switch>
+        <AppRoute restrictedTo={GUEST} exact path="/login" component={Login} />
+
+        <AppRoute restrictedTo={GUEST} exact path="/signup" component={Signup} />
+
+        <AppRoute restrictedTo={AUTHENTICATED} exact path="/" component={Home} />
+
+        <AppRoute restrictedTo={AUTHENTICATED} exact path="/reserve" component={ReserveTab} />
+      </Switch>
+    </AuthProvider>
+  );
 }
 
 export default App;
