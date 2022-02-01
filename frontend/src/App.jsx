d4b0ef53fe650dcff8eb9af949eb9dfd4e0c1ff7
Parth Patel 2 <201501015@daiict.ac.in>
2022-02-01 12:10:47 +0530

    Frontend - Add all routes in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {Switch} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import AppRoute from './components/common/AppRoute';
 
 import Navbar from './components/common/Navbar';
+import Error404 from './pages/errors/Error404';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Manager from './pages/manager/Manager';
 import ReserveTab from './pages/reserve/ReserveTab';
 import Signup from './pages/signup/Signup';
 import AuthProvider from './providers/AuthProvider';
-import {AUTHENTICATED, GUEST} from './utils/constant';
+import {AUTHENTICATED, GUEST, MANAGER} from './utils/constant';
 
 function App() {
   return (
@@ -22,6 +24,10 @@ function App() {
         <AppRoute restrictedTo={AUTHENTICATED} exact path="/" component={Home} />
 
         <AppRoute restrictedTo={AUTHENTICATED} exact path="/reserve" component={ReserveTab} />
+
+        <AppRoute restrictedTo={MANAGER} exact path="/manager" component={Manager} />
+
+        <Route path="*" component={Error404} />
       </Switch>
     </AuthProvider>
   );
